b42f7cff62c38b9344d063880a15f5e5c41ba583
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-11-28 17:37:16 -0500

    combining the web app and electron to unify both in a single branch

--- scripts/default_devices.js
+++ scripts/default_devices.js
@@ -1,119 +1,103 @@
+// if you need more defautl devices, just add them here in this default_mudfiles variable with some key
 default_mudfiles = {
     "1": {
         "ietf-mud:mud": {
             "mud-version": 1,
-            "mud-url": "https://dell.com/PC",
-            "last-update": "2019-09-03T21:01:15+00:00",
+            "mud-url": "https://www.fridgemaker.org/smartfridge",
+            "last-update": "2019-11-28T20:38:40+00:00",
             "cache-validity": 48,
             "is-supported": true,
-            "systeminfo": "This is a personal PC ",
-            "mfg-name": "DELL",
-            "documentation": "https://dell.com/docs",
-            "model-name": "PC",
+            "systeminfo": "This is a smart fridge",
+            "mfg-name": "FridgeMakers LLC.",
+            "documentation": "https://www.fridgedocs.com/smartfridge",
+            "model-name": "smartfridge",
             "from-device-policy": {
                 "access-lists": {
-                    "access-list": [
+                    "access-list": [{
+                        "name": "mud-16483-v4fr"
+                    },
                         {
-                            "name": "mud-80713-v4fr"
-                        },
-                        {
-                            "name": "mud-80713-v6fr"
+                            "name": "mud-16483-v6fr"
                         }
                     ]
                 }
             },
             "to-device-policy": {
                 "access-lists": {
-                    "access-list": [
+                    "access-list": [{
+                        "name": "mud-16483-v4to"
+                    },
                         {
-                            "name": "mud-80713-v4to"
-                        },
-                        {
-                            "name": "mud-80713-v6to"
+                            "name": "mud-16483-v6to"
                         }
                     ]
                 }
             }
         },
         "ietf-access-control-list:acls": {
-            "acl": [
-                {
-                    "name": "mud-80713-v4to",
-                    "type": "ipv4-acl-type",
-                    "aces": {
-                        "ace": [
-                            {
-                                "name": "cl0-todev",
-                                "matches": {
-                                    "ipv4": {
-                                        "ietf-acldns:src-dnsname": "www.google.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
-                            },
-                            {
-                                "name": "cl1-todev",
-                                "matches": {
-                                    "ipv4": {
-                                        "ietf-acldns:src-dnsname": "www.amazon.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
-                            },
-                            {
-                                "name": "loc0-todev",
-                                "matches": {
-                                    "ietf-mud:mud": {
-                                        "local-networks": [
-                                            null
-                                        ]
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
+            "acl": [{
+                "name": "mud-16483-v4to",
+                "type": "ipv4-acl-type",
+                "aces": {
+                    "ace": [{
+                        "name": "cl0-todev",
+                        "matches": {
+                            "ipv4": {
+                                "ietf-acldns:src-dnsname": "www.fridgemakers.com"
                             }
-                        ]
-                    }
-                },
-                {
-                    "name": "mud-80713-v4fr",
-                    "type": "ipv4-acl-type",
-                    "aces": {
-                        "ace": [
-                            {
-                                "name": "cl0-frdev",
-                                "matches": {
-                                    "ipv4": {
-                                        "ietf-acldns:dst-dnsname": "www.google.com"
-                                    }
+                        },
+                        "actions": {
+                            "forwarding": "accept"
+                        }
+                    },
+                        {
+                            "name": "cl1-todev",
+                            "matches": {
+                                "ipv4": {
+                                    "ietf-acldns:src-dnsname": "updates.fridgemakers.com",
+                                    "protocol": 6
                                 },
-                                "actions": {
-                                    "forwarding": "accept"
+                                "tcp": {
+                                    "source-port": {
+                                        "operator": "eq",
+                                        "port": 443
+                                    }
                                 }
                             },
+                            "actions": {
+                                "forwarding": "accept"
+                            }
+                        }
+                    ]
+                }
+            },
+                {
+                    "name": "mud-16483-v4fr",
+                    "type": "ipv4-acl-type",
+                    "aces": {
+                        "ace": [{
+                            "name": "cl0-frdev",
+                            "matches": {
+                                "ipv4": {
+                                    "ietf-acldns:dst-dnsname": "www.fridgemakers.com"
+                                }
+                            },
+                            "actions": {
+                                "forwarding": "accept"
+                            }
+                        },
                             {
                                 "name": "cl1-frdev",
                                 "matches": {
                                     "ipv4": {
-                                        "ietf-acldns:dst-dnsname": "www.amazon.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
-                            },
-                            {
-                                "name": "loc0-frdev",
-                                "matches": {
-                                    "ietf-mud:mud": {
-                                        "local-networks": [
-                                            null
-                                        ]
+                                        "ietf-acldns:dst-dnsname": "updates.fridgemakers.com",
+                                        "protocol": 6
+                                    },
+                                    "tcp": {
+                                        "destination-port": {
+                                            "operator": "eq",
+                                            "port": 443
+                                        }
                                     }
                                 },
                                 "actions": {
@@ -124,39 +108,32 @@ default_mudfiles = {
                     }
                 },
                 {
-                    "name": "mud-80713-v6to",
+                    "name": "mud-16483-v6to",
                     "type": "ipv6-acl-type",
                     "aces": {
-                        "ace": [
-                            {
-                                "name": "cl0-todev",
-                                "matches": {
-                                    "ipv6": {
-                                        "ietf-acldns:src-dnsname": "www.google.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
+                        "ace": [{
+                            "name": "cl0-todev",
+                            "matches": {
+                                "ipv6": {
+                                    "ietf-acldns:src-dnsname": "www.fridgemakers.com"
                                 }
                             },
+                            "actions": {
+                                "forwarding": "accept"
+                            }
+                        },
                             {
                                 "name": "cl1-todev",
                                 "matches": {
                                     "ipv6": {
-                                        "ietf-acldns:src-dnsname": "www.amazon.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
-                            },
-                            {
-                                "name": "loc0-todev",
-                                "matches": {
-                                    "ietf-mud:mud": {
-                                        "local-networks": [
-                                            null
-                                        ]
+                                        "ietf-acldns:src-dnsname": "updates.fridgemakers.com",
+                                        "protocol": 6
+                                    },
+                                    "tcp": {
+                                        "source-port": {
+                                            "operator": "eq",
+                                            "port": 443
+                                        }
                                     }
                                 },
                                 "actions": {
@@ -167,39 +144,32 @@ default_mudfiles = {
                     }
                 },
                 {
-                    "name": "mud-80713-v6fr",
+                    "name": "mud-16483-v6fr",
                     "type": "ipv6-acl-type",
                     "aces": {
-                        "ace": [
-                            {
-                                "name": "cl0-frdev",
-                                "matches": {
-                                    "ipv6": {
-                                        "ietf-acldns:dst-dnsname": "www.google.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
+                        "ace": [{
+                            "name": "cl0-frdev",
+                            "matches": {
+                                "ipv6": {
+                                    "ietf-acldns:dst-dnsname": "www.fridgemakers.com"
                                 }
                             },
+                            "actions": {
+                                "forwarding": "accept"
+                            }
+                        },
                             {
                                 "name": "cl1-frdev",
                                 "matches": {
                                     "ipv6": {
-                                        "ietf-acldns:dst-dnsname": "www.amazon.com"
-                                    }
-                                },
-                                "actions": {
-                                    "forwarding": "accept"
-                                }
-                            },
-                            {
-                                "name": "loc0-frdev",
-                                "matches": {
-                                    "ietf-mud:mud": {
-                                        "local-networks": [
-                                            null
-                                        ]
+                                        "ietf-acldns:dst-dnsname": "updates.fridgemakers.com",
+                                        "protocol": 6
+                                    },
+                                    "tcp": {
+                                        "destination-port": {
+                                            "operator": "eq",
+                                            "port": 443
+                                        }
                                     }
                                 },
                                 "actions": {
@@ -212,4 +182,4 @@ default_mudfiles = {
             ]
         }
     }
-}
+};
